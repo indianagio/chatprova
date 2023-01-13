cd5cc6b800afaefba33c7b81bfe575f1a4125b92
Giorgio Baseggio <davverofaciledaricordare@gmail.com>
2023-01-13 13:09:11 +0100

    Initial Commit

---
+++ prova hcatui/prova_hcatuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  prova_hcatuiApp.swift
+//  prova hcatui
+//
+//  Created by Giorgio Baseggio on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct prova_hcatuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
